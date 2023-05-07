d789c8af0fb198a2b22ff108f5806c3a19c131dc
Masao-Someki <masao.someki@gmail.com>
2023-05-07 17:24:45 +0900

    fixed publicPath

--- src/components/DemoHeader.vue
+++ src/components/DemoHeader.vue
@@ -4,13 +4,13 @@
       <v-row>
         <v-col cols="3">
           <a href="https://github.com/espnet/espnet">
-            <img src="https://masao-someki.github.io/espnet_onnx_demo/image/espnet-logo.png" width="100%" />
+            <img src="https://github.com/Masao-Someki/espnet_onnx_demo/blob/master/public/image/espnet-logo.png" width="100%" />
           </a>
         </v-col>
         <v-spacer></v-spacer>
         <v-col cols="1" style="text-align: right">
           <a href="https://github.com/espnet/espnet_onnx">
-            <img src="https://masao-someki.github.io/espnet_onnx_demo/image/github-mark.png" width="100%" />
+            <img src="https://github.com/Masao-Someki/espnet_onnx_demo/blob/master/public/image/github-mark.png" width="100%" />
           </a>
         </v-col>
       </v-row>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: 'https://masao-someki.github.io/espnet_onnx_demo/'
 })
